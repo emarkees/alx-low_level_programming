565b8f7cce52f049b24571beb7edffaac6cb26c0
emarkees <rphillipse@gmail.com>
2022-09-13 23:57:40 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 
 /**
  * print_sign - Prints the sign of a number.
- *@n The number of which the sign will be printed.
+ *@n The number of which the sign will be printed.n
  *
  * Return: 1 if the number is greater than zero,
  *         0 if the number is zero,
