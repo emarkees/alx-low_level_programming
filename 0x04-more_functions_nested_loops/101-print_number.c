2d1c724d716a34e18ac8e9a27d2ef38eaa25f254
emarkees <rphillipse@gmail.com>
2022-09-15 16:39:50 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -13,9 +13,9 @@ void print_number(int n)
 		if (n < 0)
 		{
 			_putchar(45);
-			x =-n;
+			x = -n;
 		}
-		if (x/10)
-			print_number(x/10);
-		_putchar((x%10) + '0');
+		if (x / 10)
+			print_number(x / 10);
+		_putchar((x % 10) + '0');
 	}
