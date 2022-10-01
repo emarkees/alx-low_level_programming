a0f8c3cc5ce575c6d3681b1776524fbcd1d59ca9
emarkees <rphillipse@gmail.com>
2022-09-30 17:28:39 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,10 +10,4 @@ int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
-}
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
+}y
